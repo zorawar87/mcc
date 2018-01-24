4de6d611eda606fb4afcbe51946fac6827f37180
Zorawar Moolenaar <zorawar87@gmail.com>
2018-01-23 21:54:46 -0500

    updates

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	templates := template.Must(template.ParseFiles("templates/index.html"))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if err := templates.ExecuteTemplate(w, "index.html", nil); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+	http.ListenAndServe(":80", nil)
+}
